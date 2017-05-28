a9a972b46b28ec847242a700180162b77326b82f
Douglas Lyford <Douggyfr3sh@gmail.com>
2017-05-27 20:54:47 -0400

    Completed all minimum requirements.  Would like to improve front end appearance and behavior.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,11 +1,15 @@
 // YOUR CODE HERE:
 //$(document).ready(function() {
   var app = {
-    friends: [],
+    friends: {},
+    server: 'http://parse.hrr.hackreactor.com/chatterbox/classes/messages',
+    roomName: 'lobby',
+    messages: [],
+    roomList: {},
 
     send: function(message) {
       $.ajax({
-        url: 'http://parse.hrr.hackreactor.com/chatterbox/classes/messages',
+        url: app.server,
         type: 'POST',
         data: JSON.stringify(message),
         contentType: 'application/json',
@@ -21,17 +25,32 @@
     },
 
     init: function() {
-      $('.room-display').text('hrr24');
+      $('.room-display').text(app.roomName);
+      app.fetch();
+
+
+      setInterval(function() {
+        app.fetch();
+      }, 3000);
     },
 
     fetch: function() {
       var fetchObj = $.ajax({
-        url: 'http://parse.hrr.hackreactor.com/chatterbox/classes/messages',
+        url: app.server,
         type: 'GET',
+        data: {order: '-createdAt'},
         //data: JSON.stringify(message),
-        contentType: 'application/json',
+        //contentType: 'application/json',
         success: function (data) {
-          console.log('chatterbox: Fetch executed');
+          // if (!data || !data[0]) {
+          //   return;
+          // }
+          //update dom if new messages are available
+          //if (mostRecentMessage.objectId !== app.lastMessageId)
+          app.messages = data.results.slice();
+          app.renderMessages(data.results, app.roomName);
+          app.renderRoomList(app.messages);
+
         },
         error: function (data) {
           // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -39,7 +58,7 @@
         }
       });
 
-      return fetchObj.responseJSON;
+      //return fetchObj;
       //grab the array of messages  ---> return it
       //iterate over the array of messages
         //call app.renderMessage(messages[i])
@@ -49,40 +68,78 @@
       $('#chats').children().remove();
     },
 
-    renderMessage: function(message) {
+    renderMessages: function(messages, roomName) {
+      $('#chats').html('');
+
+      messages.forEach(function(message) {
+
+        app.renderMessage(message, roomName);
+      });
+    },
+
+    renderMessage: function(message, roomName) {
+      //app.messages.unshift(message);
+      if (message.roomname !== roomName) {
+        return;
+      }
+      // if (app.messages.length > 100) {
+      //   app.messages.pop();
+      // }
       var $newMsg = $(
         `<div class="panel panel-default">
-          <div class="panel-heading username">${message.username}
+          <div class="panel-heading username">
           </div>
-          <div class="panel-body">${message.text}</div>
+          <div class="panel-body"></div>
         </div>
         `);
+      $newMsg.find('.panel-heading').text(message.username);
+      $newMsg.find('.panel-body').text(message.text);
 
-      var $usernameTxt = $('.username').text();
-      if (app.friends.includes($usernameTxt)) {
+      var $usernameTxt = $newMsg.find('.panel-heading').text();
+      console.log($usernameTxt);
+      if (app.friends.hasOwnProperty($usernameTxt)) {
         $newMsg.find('.panel-heading').addClass('friend');
       }
 
+      $newMsg.text();
       $('#chats').prepend($newMsg);
 
-      $('.username').on('click', function () {
+      $newMsg.find('.username').on('click', function () {
         console.log('username was clicked');
-        app.handleUsernameClick($('.username').text());
+        app.handleUsernameClick($newMsg.find('.username').text());
       });
     },
 
     renderRoom: function (roomName) {
       $newRoom = $('<li><a href="#"></a></li>');
       $newRoom.children().text(roomName);
-      $('#roomSelect').prepend($newRoom);
-      $('.room-display').text(roomName);
+      $newRoom.on('click', function() {
+        app.roomName = roomName;
+        $('.room-display').text(app.roomName);
+        app.renderMessages(app.messages, app.roomName);
+      });
 
+      $('#roomSelect').prepend($newRoom);
+
+    },
+
+    renderRoomList: function(messages) {
+      if (messages.length > 0) {
+        messages.forEach(function(message) {
+          if (!app.roomList.hasOwnProperty(message.roomname)) {
+            app.roomList[message.roomname] = true;
+            app.renderRoom(message.roomname);
+          }
+        });
+      }
     },
 
     handleUsernameClick: function(username) {
       var friends = this.friends;
-      if (!friends.includes(username)) {
-        friends.push(username);
+      if (!friends.hasOwnProperty(username)) {
+        friends[username] = true;
+      } else {
+        delete friends[username];
       }
     },
 
@@ -90,13 +147,15 @@
       var message = {};
       message.username = window.location.search.slice(10);
       message.text = $('#message')[0].value;
-      message.roomname = $('.room-display').text();
+      message.roomname = app.roomName;
 
-      //this.send(message);
+      this.send(message);
       console.log('handleSubmit was called: ', message);
 
       $('#message')[0].value = '';
     }
+
+
 
 
   };
@@ -111,6 +170,7 @@
     var $roomDisplay = $('.room-display'); //displays room name
     var message = undefined;
     //var username = window.location.search.slice(10);
+    app.init();
     $submitBtn.on('click', function() {
       console.log('submit button clicked, message is:', $('#message')[0].value);
       if ($('#message')[0].value !== undefined) {
@@ -121,7 +181,13 @@
 
     $('.create-room').on('click', function() {
       console.log('click got there.');
-      app.renderRoom(prompt('Enter room name'));
+      var newRoom = prompt('Enter room name');
+      if (!app.roomList.hasOwnProperty(newRoom)) {
+        app.renderRoom(newRoom);
+        app.roomList[newRoom] = true;
+      }
+
+
     });
 
   });
